2eb9082e5b208fb3447f14e4014018c8ee691492
arielberenson <berenson123@gmail.com>
2025-03-11 15:06:50 +0200

    beginning of changes

--- game_server.py
+++ game_server.py
@@ -19,8 +19,7 @@ class GameServer:
         connect_user_thread.start()
         data_thread = threading.Thread(target=self.recv_data, daemon=True)
         data_thread.start()
-        process_data_thread = threading.Thread(target=self.process_data, daemon=True)
-        process_data_thread.start()
+
 
     def extract_json(self, data):
         """Extract a complete JSON object from the data, handling nested structures."""
@@ -38,20 +37,7 @@ class GameServer:
                     return data[i + 1:], data[start_idx:i + 1]  # Return the remaining data and the JSON object
         return data, ""  # If no complete JSON is found, return the data unprocessed
 
-    def send_to_all(self, recipients, m):
-        for recipient in recipients:
-            try:
-                us = recipient.get_user()
-                us.get_socket().sendall(m.encode('utf-8'))
-            except Exception as e:
-                recipient.get_socket().sendall(m.encode('utf-8'))
 
-    def create_message(self, message_type, data1, data2):
-        return json.dumps({
-            "type": message_type,
-            "data1": data1,
-            "data2": data2,
-        })
 
     def create_game(self, user):
         p = Player(user, user.get_username(), 1000)
@@ -60,6 +46,11 @@ class GameServer:
         self.games[game_id] = new_game
         self.game_counter += 1  # Increment the counter for the next game
         print(f"Game {game_id} created.")
+        new_game_thread = threading.Thread(target=self.run_game, args=(game_id,), daemon=True)
+        new_game_thread.start()
+
+    def run_game(self, game_id):
+        self.games[game_id].start_game()
 
 
     def start_server(self):
@@ -103,87 +94,7 @@ class GameServer:
                         self.message_queue.put(message, current_game)
                 time.sleep(0.1)
 
-    def process_data(self):
-        print('process started')
-        while True:
-            try:
-                incoming_message, current_game = self.message_queue.get_nowait()  # Non-blocking get from the queue
-                print("RAW message: ", incoming_message)
-                extra, incoming_message = self.extract_json(incoming_message)
-                print("extra", extra)
-                if extra:
-                    self.message_queue.put(extra)
-                message_data = json.loads(incoming_message)
-                message_type = message_data.get("type")
-                print("JSON message: ", message_data)
-                data1 = message_data.get("data1")
-                data2 = message_data.get("data2")
 
-                if message_type == 'create':
-                    for user in self.users.get_users():
-                        print('username: ', user.get_username())
-                        if user.get_username() == data1:
-                            self.create_game(user)
-                            message = self.create_message('approve', 'create', '')
-                            user.get_socket().sendall(message.encode('utf-8'))
-                            message = self.create_message('new_game', user.get_username(), '')
-                            self.send_to_all(self.users.get_users(), message)
 
-                if message_type == 'join':
-                    for user in self.users.get_users():
-                        if user.get_username() == data1:
-                            message = self.create_message('player-joined', 'lobby', data1)
-                            self.send_to_all(current_game.get_players(), message)
-                            p = Player(user, user.get_username(), 1000)
-                            players.add_players(p)
-                            names = []
-                            for p in players.get_players():
-                                names.append(p.get_username())
-                            message = self.create_message('approve', 'join', names)
-                            user.get_socket().sendall(message.encode('utf-8'))
-
-                if message_type == 'game-start':
-                    game_start = True
-                    message = self.create_message('approve', 'game-start', '')
-                    self.send_to_all(players.get_players(), message)
-                    self.game_queue.put(players)
-
-                if message_type == 'name':
-                    for u in self.users.get_users():
-                        print("real", u.get_address())
-                        print("sent", data2)
-                        if u.get_address() == tuple(data2):
-                            u.set_username(data1)
-                            print('for ', data2, ' accepted ', data1)
-
-                if message_type == 'player_move':
-                    if data1 == 'call':
-                        print("players last bet: ", players.get_last_bet())
-                        place_bet(players, pot, players.get_last_bet())
-                        message = self.create_message('player chips', players.get_current().get_name(),
-                                                 players.get_current().get_chips())
-                        self.send_to_all(players.get_players(), message)
-                        message = self.create_message('pot', pot.get_chips(), '')
-                        self.send_to_all(players.get_players(), message)
-                    elif data1 == 'raise':
-                        place_bet(players, pot, data2)
-                        message = self.create_message('player chips', players.get_current().get_name(),
-                                                 players.get_current().get_chips())
-                        self.send_to_all(players.get_players(), message)
-                        message = self.create_message('pot', pot.get_chips(), '')
-                        self.send_to_all(players.get_players(), message)
-                        players.player_raised()
-                    elif data1 == 'fold':
-                        fold(players)
-                    players.next_player()
-                    print("Turn count: ", players.get_turn_counter())
-                    print("Max Turns: ", players.get_max_turns())
-                    game_queue.put((data1, data2))
-
-            except queue.Empty:
-                pass  # No message to process, simply skip
-
-            except Exception as e:
-                print(f"An error occurred: {e}")
 
 
